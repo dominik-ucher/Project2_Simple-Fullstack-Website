36f0ec96ca732fe78ff81292891117edc41f9ce0
Dominik Ucher <dominikucher@hotmail.com>
2023-07-05 15:14:10 +0200

    Homepage works

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Logo from '../../img/logo.png'
 import { Link } from 'react-router-dom'
-import './Home.css'
+import { Button } from 'flowbite-react';
 
   const posts = [
     {
@@ -34,33 +34,116 @@ const Home = () => {
     return(
         <>
         <img
-        classname="h-auto max-w-full"
+        className="object-cover h-auto w-1024"
         src="https://scontent.fosl3-2.fna.fbcdn.net/v/t39.30808-6/342891160_127035133686987_4188360982766614032_n.jpg?_nc_cat=104&cb=99be929b-3346023f&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=1p9q7BNAOtIAX9_8LOq&_nc_ht=scontent.fosl3-2.fna&oh=00_AfB_YN1eMWEmd-kCIspubdMku2xSN17IVSljBfbT7AKfxQ&oe=64AA1AE7"
         alt="image description"
         />
-        <div class="relative w-12/14 mx-auto p-4 bg-gray-800 text-white rounded-xl justify-center mt-3">
-        <p class="text-lg justify-center flex items-center">Velkommen til Idrettslaget Trond</p>
-        <p class="text-ls mt-2 justify-center flex items-center">Artig-Inkluderende-Utviklende</p>
+        <div className="relative w-12/14 mx-auto p-4 bg-gray-800 text-white rounded-xl justify-center mt-3">
+        <p className="text-lg justify-center flex items-center">Velkommen til Idrettslaget Trond</p>
+        <p className="text-ls mt-2 justify-center flex items-center">Artig-Inkluderende-Utviklende</p>
         </div>
-        
-        <div className="home">
-        <div className="posts">
-            {posts.map((post) => (
-            <div className="post" key={post.id}>
-                <div className="img">
-                <img src={post.img} alt="" />
+
+        <div className="grid grid-cols-10 gap-4">
+        <div className="col-span-6 bg-gray-200">
+          {/* Left Column */}
+          <div className="p-4 flex items-center justify-between">
+            {/* First iteration - Image on the right, text on the left */}
+            <div className="w-1/2">
+              {/* Image content */}
+              <img
+                className=""
+                src="https://scontent.fosl3-2.fna.fbcdn.net/v/t39.30808-6/342891160_127035133686987_4188360982766614032_n.jpg?_nc_cat=104&cb=99be929b-3346023f&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=1p9q7BNAOtIAX9_8LOq&_nc_ht=scontent.fosl3-2.fna&oh=00_AfB_YN1eMWEmd-kCIspubdMku2xSN17IVSljBfbT7AKfxQ&oe=64AA1AE7"
+                alt="image description"
+                />
+            </div>
+            <div className="w-1/2">
+              {/* Text content */}
+              <p>Hello how is it going asjkdhas djkashd jkhasd jkhasdkjd h jaskdhjksdakjdsha  jadshkasjd jkashd jsakdhjaksd</p>
+            </div>
+          </div>
+          
+        </div>
+        <div className="col-span-4 bg-gray-300">
+          {/* Right Column */}
+          <div className="p-4">
+            <div className="grid grid-cols-2 gap-4">
+              {/* First Row */}
+              <div className="col-span-2">
+                {/* Content for the first row */}
+                <div className="relative w-12/14 mx-auto p-4 bg-blue-500 text-white rounded-xl justify-center mt-3">
+                <p className="text-lg justify-center flex items-center">Følg oss på Facebook</p>
                 </div>
-                <div className="content">
-                <Link className="link" to={`/post/${post.id}`}>
-                    <h1>{post.title}</h1>
+              </div>
+              {/* Second Row */}
+              <div className="col-span-2">
+                {/* Content for the second row */}
+                <p className="text-xl justify-center flex items-center">Nyttig Informasjon</p>
+              </div>
+              {/* Third Row */}
+              <div className="col-span-2">
+                {/* Content for the third row */}
+                <img
+                className="object-cover"
+                src="https://scontent.fosl3-2.fna.fbcdn.net/v/t39.30808-6/342891160_127035133686987_4188360982766614032_n.jpg?_nc_cat=104&cb=99be929b-3346023f&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=1p9q7BNAOtIAX9_8LOq&_nc_ht=scontent.fosl3-2.fna&oh=00_AfB_YN1eMWEmd-kCIspubdMku2xSN17IVSljBfbT7AKfxQ&oe=64AA1AE7"
+                alt="image description"
+                />
+              </div>
+              {/* Fourth Row */}
+              <div className="col-span-2">
+                {/* Content for the Fourth row */}
+                <img
+                className="object-cover"
+                src="https://scontent.fosl3-2.fna.fbcdn.net/v/t39.30808-6/342891160_127035133686987_4188360982766614032_n.jpg?_nc_cat=104&cb=99be929b-3346023f&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=1p9q7BNAOtIAX9_8LOq&_nc_ht=scontent.fosl3-2.fna&oh=00_AfB_YN1eMWEmd-kCIspubdMku2xSN17IVSljBfbT7AKfxQ&oe=64AA1AE7"
+                alt="image description"
+                />
+              </div>
+              {/* Additional Rows */}
+              {/* Add more div elements with the desired content for additional rows */}
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+        {/* <div className='flex home_page mt-3'>
+          <div className='news_list flex-5 mt-3'>
+            {posts.map((posts) => (
+            <div className='news box-border h-auto w-auto p-4 border-4' key={posts.id}>
+              <div className='img'>
+                <img className="object-right object-scale-down h-48 w-full" src={posts.img} alt="" />
+              </div>
+              <div className='content'>
+                <Link className='link' to={posts.id}>
+                  <h1>{posts.title}</h1>
                 </Link>
-                <p>{post.desc}</p>
+                <p>{posts.desc}</p>
                 <button>Read More</button>
-                </div>
+              </div>
             </div>
             ))}
-        </div>
-        </div>
+          </div>
+          <div className='flex-2 menu'>
+            <div className="relative w-12/14 mx-auto p-4 bg-blue-500 text-white rounded-xl justify-center mt-3">
+            <p className="text-lg justify-center flex items-center">Følg oss på Facebook</p>
+            </div>
+            <h1 className='info'>Nyttig Informasjon</h1>
+            {posts.map((post) => (
+            <div className="post" key={post.id}>
+              <img className='menu_img' src={post.img} alt="" />
+              <h2>{post.title}</h2>
+              <button>Read More</button>
+            </div>
+            ))}
+          </div>
+        </div> */}
         </>
     )
 }
