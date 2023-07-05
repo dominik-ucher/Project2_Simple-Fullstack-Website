45ab602dc030cc71169d81cc2175c3ccdfc79b80
Dominik Ucher <dominikucher@hotmail.com>
2023-07-05 15:43:14 +0200

    Home ser bra ut!

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -46,22 +46,21 @@ const Home = () => {
         <div className="grid grid-cols-10 gap-4">
         <div className="col-span-6 bg-gray-200">
           {/* Left Column */}
-          <div className="p-4 flex items-center justify-between">
+          {posts.map((post,index) => (
+          <div key={post.id} className={`p-4 flex items-center justify-between ${index % 2 === 0 ? 'flex-row':'flex-row-reverse'} `}>
             {/* First iteration - Image on the right, text on the left */}
             <div className="w-1/2">
               {/* Image content */}
-              <img
-                className=""
-                src="https://scontent.fosl3-2.fna.fbcdn.net/v/t39.30808-6/342891160_127035133686987_4188360982766614032_n.jpg?_nc_cat=104&cb=99be929b-3346023f&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=1p9q7BNAOtIAX9_8LOq&_nc_ht=scontent.fosl3-2.fna&oh=00_AfB_YN1eMWEmd-kCIspubdMku2xSN17IVSljBfbT7AKfxQ&oe=64AA1AE7"
-                alt="image description"
-                />
+              <img src={post.img} alt="" className='h-48 w-48 mt-3'/>
             </div>
             <div className="w-1/2">
               {/* Text content */}
-              <p>Hello how is it going asjkdhas djkashd jkhasd jkhasdkjd h jaskdhjksdakjdsha  jadshkasjd jkashd jsakdhjaksd</p>
+              <h1 className='text-lg underline decoration-1 font-bold'>{post.title}</h1>
+              <h2>{post.desc}</h2>
+              <Button gradientDuoTone='redToYellow' outline className="flex items-center justify-center"><h2>Les mer</h2></Button>
             </div>
           </div>
-          
+          ))}
         </div>
         <div className="col-span-4 bg-gray-300">
           {/* Right Column */}
@@ -77,7 +76,7 @@ const Home = () => {
               {/* Second Row */}
               <div className="col-span-2">
                 {/* Content for the second row */}
-                <p className="text-xl justify-center flex items-center">Nyttig Informasjon</p>
+                <h1 className="text-xl font-bold justify-center flex items-center">Nyttig Informasjon</h1>
               </div>
               {/* Third Row */}
               <div className="col-span-2">
