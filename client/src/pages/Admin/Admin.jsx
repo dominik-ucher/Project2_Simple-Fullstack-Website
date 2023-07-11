b54bbc69cbb1cf68af979e51a096322189c05fcd
Dominik Ucher <dominikucher@hotmail.com>
2023-07-11 23:01:28 +0200

    good

--- client/src/pages/Admin/Admin.jsx
+++ client/src/pages/Admin/Admin.jsx
@@ -55,6 +55,14 @@ const Admin = () => {
                 <p>Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
                 </p>
             </Card>
+            <Card className="max-w-sm mt-5" href="#">
+                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <p>Rediger brukere</p>
+                </h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                <p>Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                </p>
+            </Card>
                     
             </div>
             <div className="col-span-10 md:col-span-5 bg-white-200 flex flex-col items-front mt-20">
