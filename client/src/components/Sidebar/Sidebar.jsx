f88039d90100095003325067505c3a6efe66108c
Dominik Ucher <dominikucher@hotmail.com>
2023-08-01 00:40:44 +0200

    New sidebar, dropped flowbite

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,33 +1,115 @@
-// Sidebar.js
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Sidebar as FlowbiteSidebar } from 'flowbite-react'; // Renamed import
+import React, { useState } from 'react';
+import './Sidebar.css';
 
 const SidebarComponent = () => {
+  const [dropdowns, setDropdowns] = useState([
+    {
+      id: 1,
+      label: 'E-commerce',
+      isOpen: false,
+      submenus: [
+        { id: 11, label: 'Products', isOpen: false, submenus: ['Product 1', 'Product 2'] },
+        { id: 12, label: 'Sales', isOpen: false, submenus: ['Sale 1', 'Sale 2'] },
+      ],
+    },
+    {
+      id: 2,
+      label: 'Settings',
+      isOpen: false,
+      submenus: [
+        { id: 21, label: 'General', isOpen: false, submenus: ['Setting 1', 'Setting 2'] },
+        { id: 22, label: 'Security', isOpen: false, submenus: ['Security 1', 'Security 2'] },
+        { id: 23, label: 'Notifications', isOpen: false, submenus: ['Notification 1', 'Notification 2'] },
+      ],
+    },
+    {
+      id: 3,
+      label: 'Reports',
+      isOpen: false,
+      submenus: [
+        { id: 31, label: 'Sales Report', isOpen: false, submenus: ['Report 1', 'Report 2'] },
+        { id: 32, label: 'Inventory Report', isOpen: false, submenus: ['Report 3', 'Report 4'] },
+      ],
+    },
+  ]);
+
+  const toggleDropdown = (parentId, submenuId) => {
+    setDropdowns((prevDropdowns) =>
+      prevDropdowns.map((parentDropdown) => {
+        if (parentDropdown.id === parentId) {
+          return {
+            ...parentDropdown,
+            isOpen: submenuId === null ? !parentDropdown.isOpen : parentDropdown.isOpen,
+            submenus: parentDropdown.submenus.map((submenu) =>
+              submenu.id === submenuId ? { ...submenu, isOpen: !submenu.isOpen } : { ...submenu, isOpen: false }
+            ),
+          };
+        } else {
+          return {
+            ...parentDropdown,
+            isOpen: false,
+            submenus: parentDropdown.submenus.map((submenu) => ({ ...submenu, isOpen: false })),
+          };
+        }
+      })
+    );
+  };
 
   return (
-    <div className='flex justify-center item-center mt-5'>
-      <FlowbiteSidebar aria-label="Sidebar with multi-level dropdown example">
-        <FlowbiteSidebar.Items>
-          <FlowbiteSidebar.ItemGroup>
-            <FlowbiteSidebar.Item href="#"><p>Dashboard</p></FlowbiteSidebar.Item>
-
-            <FlowbiteSidebar.Collapse label="E-commerce">
-              <FlowbiteSidebar.Item>Products</FlowbiteSidebar.Item>
-              <FlowbiteSidebar.Item>Sales</FlowbiteSidebar.Item>
-              <FlowbiteSidebar.Collapse label="E-commerce">
-                <FlowbiteSidebar.Item>Products</FlowbiteSidebar.Item>
-                <FlowbiteSidebar.Item>Sales</FlowbiteSidebar.Item>
-              </FlowbiteSidebar.Collapse>
-            </FlowbiteSidebar.Collapse>
-
-            <FlowbiteSidebar.Collapse label="E-commerce">
-              <FlowbiteSidebar.Item>Products</FlowbiteSidebar.Item>
-              <FlowbiteSidebar.Item>Sales</FlowbiteSidebar.Item>
-            </FlowbiteSidebar.Collapse>
-          </FlowbiteSidebar.ItemGroup>
-        </FlowbiteSidebar.Items>
-      </FlowbiteSidebar>
+    <div className='sidebar'>
+      <div className='sidebar-header'>
+        <h3>Dashboard</h3>
+      </div>
+      <div className='sidebar-menu'>
+        <div className='sidebar-item'>
+          <p>Dashboard</p>
+        </div>
+        {dropdowns.map((parentDropdown) => (
+          <div key={parentDropdown.id}>
+            <div
+              className={`sidebar-item ${parentDropdown.isOpen ? 'active' : ''}`}
+              onClick={() => toggleDropdown(parentDropdown.id, null)}
+            >
+              <div className="arrow-container">
+                <p>{parentDropdown.label}</p>
+                <i className={`arrow ${parentDropdown.isOpen ? 'up' : 'down'}`}></i>
+                <div className={`arrow-right ${parentDropdown.isOpen ? 'visible' : ''}`}></div>
+              </div>
+            </div>
+            {parentDropdown.isOpen && (
+              <div className='dropdown-menu'>
+                {parentDropdown.submenus.map((submenu) => (
+                  <div key={submenu.id}>
+                    <div
+                      className={`submenu-item ${submenu.isOpen ? 'active' : ''}`}
+                      onClick={() => toggleDropdown(parentDropdown.id, submenu.id)}
+                    >
+                      <p>{submenu.label}</p>
+                      <i className={`arrow ${submenu.isOpen ? 'up' : 'down'}`}></i>
+                      <div className={`arrow-right ${submenu.isOpen ? 'visible' : ''}`}></div>
+                    </div>
+                    {submenu.isOpen && (
+                      <div className='sub-submenu'>
+                        {submenu.submenus.map((subSubmenu) => (
+                          <div className='sub-submenu-item' key={subSubmenu}>
+                            <p>{subSubmenu}</p>
+                          </div>
+                        ))}
+                      </div>
+                    )}
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        ))}
+        <div className='sidebar-item'>
+          <p>Products</p>
+        </div>
+        <div className='sidebar-item'>
+          <p>Sales</p>
+        </div>
+      </div>
     </div>
   );
 };
