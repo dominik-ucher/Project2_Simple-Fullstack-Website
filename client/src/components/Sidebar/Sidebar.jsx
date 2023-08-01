eb7cc42b7ef048c770801084317757ba497b3a6c
Dominik Ucher <dominikucher@hotmail.com>
2023-08-01 21:12:07 +0200

    Sidebar working

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,15 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { BiChevronDown, BiChevronUp } from 'react-icons/bi';
 import './Sidebar.css';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const SidebarComponent = () => {
   const navigate = useNavigate();
+  const location = useLocation();
   const [sidebarMenus, setSidebarMenus] = useState([]);
   const [pages, setPages] = useState([]);
+  const [activeMenuId, setActiveMenuId] = useState(null);
 
   useEffect(() => {
     // Fetch the sidebar menu items from the backend
@@ -30,42 +33,72 @@ const SidebarComponent = () => {
       .catch((error) => {
         console.error('Error fetching pages', error);
       });
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    // Get the current article's menu and set its dropdown to open
+    const currentPath = location.pathname;
+    const currentPage = pages.find((page) => currentPath.includes(`/side/${page.id}`));
+    if (currentPage) {
+      setActiveMenuId(currentPage.sidebar_id);
+    }
+  }, [pages, location.pathname]);
+
+  const [dropdownOpen, setDropdownOpen] = useState({});
+
+  const toggleDropdown = (menuId) => {
+    setDropdownOpen((prevState) => ({
+      ...prevState,
+      [menuId]: !prevState[menuId],
+    }));
+  };
 
   const renderMenuItems = (menus, parentId = null, level = 0) => {
     return menus
       .filter((menu) => menu.parent_id === parentId)
       .map((menu) => (
         <div key={menu.id} className="pl-4">
-          <div className="flex items-center gap-4">
+          <div
+            className="flex items-center gap-4 cursor-pointer hover:bg-gray-100"
+            onClick={() => toggleDropdown(menu.id)}
+          >
             <span>{`${'-'.repeat(level)} ${menu.name}`}</span>
+            {dropdownOpen[menu.id] ? (
+              <BiChevronUp className="text-gray-500" />
+            ) : (
+              <BiChevronDown className="text-gray-500" />
+            )}
           </div>
           {/* Render pages under the menu */}
-          <div className="pl-8">
-            {pages
-              .filter((page) => page.sidebar_id === menu.id)
-              .map((page) => (
-                <div key={page.id} className="pl-2">
-                  <span onClick={() => navigate(`/side/${page.id}`)} className="cursor-pointer">
+          {dropdownOpen[menu.id] && (
+            <div className="pl-8">
+              {pages
+                .filter((page) => page.sidebar_id === menu.id)
+                .map((page) => (
+                  <div
+                    key={page.id}
+                    className={`pl-2 cursor-pointer ${
+                      location.pathname.includes(`/side/${page.id}`) ? 'font-bold' : ''
+                    }`}
+                    onClick={() => navigate(`/side/${page.id}`)}
+                  >
                     {page.title}
-                  </span>
-                </div>
-              ))}
-          </div>
+                  </div>
+                ))}
+            </div>
+          )}
           {renderMenuItems(menus, menu.id, level + 1)}
         </div>
       ));
   };
 
   return (
-    <div className='sidebar'>
+    <div className="sidebar bg-gray-200 p-4">
       <div className="flex flex-col items-center justify-center h-screen">
         <h1 className="text-center mt-8 text-2xl font-bold">Sidebar Menu</h1>
 
         {/* List of Menus */}
-        <div className="mt-5">
-          {renderMenuItems(sidebarMenus)}
-        </div>
+        <div className="mt-5">{renderMenuItems(sidebarMenus)}</div>
       </div>
     </div>
   );
