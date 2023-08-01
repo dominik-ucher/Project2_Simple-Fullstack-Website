c1bd061f1c517cae59aba82fca4aeded0ba7fb7c
Dominik Ucher <dominikucher@hotmail.com>
2023-08-01 22:11:30 +0200

    Updated admin

--- client/src/pages/Admin/Admin.jsx
+++ client/src/pages/Admin/Admin.jsx
@@ -69,9 +69,20 @@ const Admin = () => {
                 </p>
             </Card>
             </Link>
+            <Link to="http://localhost:5173/sidebarmenu_edit">
+            <Card className="max-w-sm mt-5">
+                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <p>Rediger Sidebar</p>
+                </h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                <p>Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                </p>
+            </Card>
+            </Link>
                     
             </div>
             <div className="col-span-10 md:col-span-5 bg-white-200 flex flex-col items-front mt-20">
+                <Link to="http://localhost:5173/write_page">
                 <Card className="max-w-sm" href="#">
                     <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                     <p>Legg til ny side</p>
@@ -80,6 +91,7 @@ const Admin = () => {
                     <p>Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
                     </p>
                 </Card>
+                </Link>
                 <Link to="http://localhost:5173/homepagemenu_edit">
                 <Card className="max-w-sm mt-5" href="#">
                     <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
