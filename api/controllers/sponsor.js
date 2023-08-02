ab8cee43a10f15dfff65abdf3b4c713830c4befa
Dominik Ucher <dominikucher@hotmail.com>
2023-08-02 19:31:58 +0200

    Picture delete when clicked delete

--- api/controllers/sponsor.js
+++ api/controllers/sponsor.js
@@ -1,5 +1,6 @@
 import { db } from "../db.js";
 import jwt from "jsonwebtoken";
+import fs from "fs"
 
 export const getSponsorer = (req, res) => {
   const q = "SELECT * FROM sponsor";
@@ -46,22 +47,38 @@ export const addSponsor = (req, res) => {
 };
 
 export const deleteSponsor = (req, res) => {
-  const token = req.cookies.access_token;
-  if (!token) return res.status(401).json("Not authenticated!");
-
-  jwt.verify(token, "jwtkey", (err, userInfo) => {
-    if (err) return res.status(403).json("Token is not valid!");
-
-    const sponsorId = req.params.id;
-    const q = "DELETE FROM sponsor WHERE `id` = ?";
-
-    db.query(q, [sponsorId], (err, data) => {
-      if (err) return res.status(403).json("You can delete only your sponsor!");
-
-      return res.json("Sponsor has been deleted!");
+    const token = req.cookies.access_token;
+    if (!token) return res.status(401).json("Not authenticated!");
+  
+    jwt.verify(token, "jwtkey", (err, userInfo) => {
+      if (err) return res.status(403).json("Token is not valid!");
+  
+      const sponsorId = req.params.id;
+  
+      // First, retrieve the image filename from the database
+      const getImageFilenameQuery = "SELECT img FROM sponsor WHERE id = ?";
+      db.query(getImageFilenameQuery, [sponsorId], (err, result) => {
+        if (err) return res.status(500).json(err);
+  
+        const imageFilename = result[0].img;
+  
+        // Delete the image file from storage
+        const imagePath = `../client/upload/Sponsor_Bilder/${imageFilename}`;
+        fs.unlink(imagePath, (unlinkErr) => {
+          if (unlinkErr) console.error("Error deleting image:", unlinkErr);
+  
+          // Proceed to delete the sponsor record from the database
+          const deleteQuery = "DELETE FROM sponsor WHERE id = ?";
+          db.query(deleteQuery, [sponsorId], (deleteErr, data) => {
+            if (deleteErr) return res.status(403).json("You can delete only your sponsor!");
+  
+            return res.json("Sponsor and associated image have been deleted!");
+          });
+        });
+      });
     });
-  });
-};
+  };
+  
 
 export const updateSponsor = (req, res) => {
   const token = req.cookies.access_token;
