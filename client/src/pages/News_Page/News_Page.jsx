68fc4ed76d44aaa22f4ed6d24c37b5574e1ec7cc
Dominik Ucher <dominikucher@hotmail.com>
2023-12-19 19:16:39 +0100

    News page updated

--- client/src/pages/News_Page/News_Page.jsx
+++ client/src/pages/News_Page/News_Page.jsx
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom';
 import { Button, Card, Pagination } from 'flowbite-react';
 import axios from 'axios';
 import DOMPurify from 'dompurify';
+import FadeIn from 'react-fade-in/lib/FadeIn';
+import arrow from '../../img/arrow.png'
 
 const News_Page = () => {
   const axiosInstance = axios.create({baseURL: import.meta.env.VITE_REACT_APP_API_URL,});
   const [nyheter, setNyheter] = useState([]);
   const [mainpic, setMainpic] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 8; // Number of items per page
+  const itemsPerPage = 9; // Number of items per page
 
   useEffect(() => {
     const fetchData = async () => {
@@ -40,38 +42,49 @@ const News_Page = () => {
 
   return (
     <>
-      <img
-        className="object-cover h-auto w-full"
-        key={mainpic.id}
-        src={`../upload/Homepage_Bilder/${mainpic.img}`}
-        alt="image description"
-      />
-      <div className="relative w-12/14 mx-auto p-4 bg-gray-800 text-white rounded-xl justify-center mt-3">
-        <p className="text-lg justify-center flex items-center">Velkommen til Idrettslaget Trond</p>
-        <p className="text-ls mt-2 justify-center flex items-center">Artig-Inkluderende-Utviklende</p>
-      </div>
-      <div className="grid grid-cols-10 gap-4">
-        <div className="col-span-10 md:col-span-7 bg-white-200 px-4">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            {nyheter && nyheter.slice(startIndex, endIndex).map((nyhet, index) => (
-              <Link to={`/nyheter/${nyhet.id}_${nyhet.title}`} key={nyhet.id}>
-                <div>
-                  <Card className="max-w-sm mt-10 hover:bg-gray-200">
-                    <img
-                      className="object-cover h-48 w-96"
-                      src={`../upload/Nyheter/Nyheter_Bilder/${nyhet.img}`}
-                      alt=""
-                    />
-                    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                      <p className='line-clamp-3'>{nyhet.title}</p>
-                    </h5>
-                    <div className="overflow-hidden text-gray-700 dark:text-gray-400">
-                      <div className="h-10" style={{ display: '-webkit-box', WebkitBoxOrient: 'vertical' }} dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(`<span>${nyhet.desc}</span>`) }}></div>
-                    </div>
-                    <Button gradientDuoTone='redToYellow' outline className="flex"><h2>Les mer</h2></Button>
-                  </Card>
+      <FadeIn transitionDuration={1500}>
+          <div className='p-6'>
+            <div className='relative'>
+              <img
+                className="object-cover h-auto w-full rounded-3xl"
+                key={mainpic.id}
+                src={`../upload/Homepage_Bilder/${mainpic.img}`}
+                alt="image description"
+              />
+              <div className="absolute inset-0 bg-gray-800 bg-opacity-75 text-white text-center p-4 rounded-3xl flex flex-col justify-center items-center">
+                <p className="text-xl md:text-4xl font-bold text-center">Idrettslaget Trond</p>
+                <p className="text-md md:text-2xl mt-2 font-bold text-center">Nyheter</p>
+                
+                {/* Arrow image */}
+                <div className="absolute flex justify-center bottom-0 left-0 right-0 mb-4">
+                  <img
+                    src={arrow} // Replace with your arrow image path
+                    alt="Arrow"
+                    className="h-16 w-auto animate-bounce rounded-full" // Adjust the height and width as needed
+                  />
                 </div>
-              </Link>
+              </div>
+            </div>
+          </div>
+        </FadeIn>
+        <div className='h-1 w-screen rounded-full bg-gray-300 mt-2 px-12' />
+      <div className="grid grid-cols-3 md:gap-4">
+        <div className="col-span-3 bg-white-200">
+          <div className="grid grid-cols-2 md:grid-cols-3">
+          {nyheter && nyheter.slice(startIndex,endIndex).map((nyhet, index) => (
+              <div className="flex flex-col items-center text-lg text-black font-bold hover:text-yellow-300 p-2 md:p-6">
+                <Link key={nyhet.id} to={`/nyheter/${nyhet.id}_${nyhet.title}`}>
+                <div className="relative overflow-hidden transform transition-all duration-300 rounded-lg hover:scale-105">
+                <img
+                  className="object-cover h-48 w-96 rounded-3xl"
+                  src={`../upload/Nyheter/Nyheter_Bilder/${nyhet.img}`}
+                  alt=""
+                />
+                </div>
+                <p className='line-clamp-3 p-2 flex justify-center'>{nyhet.title}</p>
+                </Link>
+                <div className='h-1 w-96 rounded-full bg-gray-300 mt-4 px-12' />
+              </div>
             ))}
           </div>
           <Pagination
@@ -80,9 +93,6 @@ const News_Page = () => {
               totalPages={Math.ceil(nyheter.length / itemsPerPage)}
               className='flex justify-center mt-10'
             />
-        </div>
-        <div className="col-span-10 md:col-span-3 bg-white-300">
-          {/* Include your Sidebar component here */}
         </div>
       </div>
     </>
