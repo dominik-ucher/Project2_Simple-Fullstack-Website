ee75462a7f920032b5027f909145cc76c5d88bca
Dominik Ucher <dominikucher@hotmail.com>
2023-12-20 22:20:52 +0100

    image and video removed

--- client/src/pages/Write_News/Write_News.jsx
+++ client/src/pages/Write_News/Write_News.jsx
@@ -83,7 +83,7 @@ const Write_News = () => {
           [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
           [{ 'font': [] }],
           [{ 'align': [] }],
-          ['image', 'video'],
+
           ['clean'] 
         ],
       };
