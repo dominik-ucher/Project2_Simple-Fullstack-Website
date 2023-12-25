c56fc949b5e4be2559029b105d02119af736ed02
Dominik Ucher <dominikucher@hotmail.com>
2023-12-25 15:17:53 +0100

    Made persons side cooler

--- client/src/pages/Personer/Personer.jsx
+++ client/src/pages/Personer/Personer.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { FaUserAlt } from "react-icons/fa";
+import { FaUserAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Personer = () => {
   const axiosInstance = axios.create({ baseURL: import.meta.env.VITE_REACT_APP_API_URL });
@@ -8,52 +9,67 @@ const Personer = () => {
   const [grupper, setGrupper] = useState([]);
 
   useEffect(() => {
-    axiosInstance.get('/api/person/')
-      .then(response => {
-        setPersoner(response.data);
-      })
-      .catch(error => {
+    const fetchData = async () => {
+      try {
+        const [personerResponse, grupperResponse] = await Promise.all([
+          axiosInstance.get('/api/person/'),
+          axiosInstance.get('/api/person/gruppe')
+        ]);
+        setPersoner(personerResponse.data);
+        setGrupper(grupperResponse.data);
+      } catch (error) {
         console.error('Error fetching data:', error);
-      });
+      }
+    };
 
-    axiosInstance.get('/api/person/gruppe')
-      .then(response => {
-        setGrupper(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
+    fetchData();
   }, []);
 
-  return (
-    <div className='flex flex-col md:grid grid-cols-4 gap-5'>
-      <div className='md:col-span-4'>
-        <h1 className='flex items-center justify-center mt-14 font-bold capitalize text-3xl md:text-4xl'>Personer i klubben</h1>
-        <div className='flex justify-center mt-3 mb-14'><div className='h-1 w-64 bg-black rounded-full' /></div>
+  const renderPersonCard = person => (
+    <div
+      key={person.id}
+      className="flex flex-col items-center justify-center border rounded-lg p-4 bg-white shadow-md transform transition duration-300 hover:scale-105 hover:shadow-xl"
+    >
+      <div className="w-48 h-48 bg-gray-200 rounded-full flex items-center justify-center overflow-hidden">
+        {person.img ? (
+          <img className="w-full h-full object-cover" src={`/upload/Personer/${person.img}`} alt="" />
+        ) : (
+          <FaUserAlt className="w-32 h-32 text-gray-500" />
+        )}
       </div>
-      {grupper && grupper.map((gruppe) => (
-        <div key={gruppe.id} className='md:col-span-4'>
-          <h1 className='flex px-10 mt-6 mb-6 font-bold capitalize text-3xl md:text-4xl'>{gruppe.navn}</h1>
-          <div className='grid grid-cols-2 md:grid-cols-4 gap-5'>
-            {personer && personer.filter(person => person.gruppe === gruppe.id).map((person) => (
-              <div key={person.id} className='flex justify-center items-center'>
-                <div className='w-64 h-auto bg-gray-200 rounded-3xl'>
-                  <div className='flex justify-center mt-3 mb-4'>
-                    {person.img ? (
-                        <img className="w-32 h-32 rounded-full object-cover" src={`/upload/Personer/${person.img}`} alt="" />
-                    ) : (
-                        <FaUserAlt className="w-32 h-32 rounded-full object-cover" />
-                    )}</div>
-                  <h1 className='flex justify-center font-bold text-lg'>{person.navn}</h1>
-                  <h1 className='flex justify-center italic text-lg'>{person.stilling}</h1>
-                  <h1 className='flex justify-center text-sm mt-2'>{person.epost}</h1>
-                  <h1 className='flex justify-center text-sm mb-3'>{person.tlf}</h1>
-                </div>
-              </div>
-            ))}
-          </div>
+      <h1 className="font-bold text-xl mt-4">{person.navn}</h1>
+      <h1 className="italic text-lg">{person.stilling}</h1>
+      <h1 className="text-sm mt-2"><a className="hover:underline" href={`mailto:${person.epost}`}>{person.epost}</a></h1>
+      <h1 className="text-sm">{person.tlf}</h1>
+    </div>
+  );
+
+  return (
+    <div className="flex flex-col md:grid grid-cols-4 gap-8">
+      <div className="md:col-span-4">
+        <h1 className="flex items-center justify-center mt-14 font-bold capitalize text-3xl md:text-4xl">Personer i klubben</h1>
+        <div className="flex justify-center mt-3 mb-14">
+          <div className="h-1 w-64 bg-black rounded-full" />
         </div>
-      ))}
+      </div>
+      {grupper &&
+        grupper.map(gruppe => (
+          <div key={gruppe.id} className="md:col-span-4">
+            <h1 className="flex px-6 mt-8 mb-6 font-bold capitalize text-3xl md:text-4xl">{gruppe.navn}</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+              {personer &&
+                personer
+                  .filter(person => person.gruppe === gruppe.id)
+                  .map(person => (
+                    <div className="group" key={person.id}>
+                      
+                        {renderPersonCard(person)}
+                    
+                    </div>
+                  ))}
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
