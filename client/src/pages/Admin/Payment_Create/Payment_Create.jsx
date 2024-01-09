10af9ab328976bdef62c8cbb7c309390503bd3cf
Dominik Ucher <dominikucher@hotmail.com>
2024-01-09 18:18:44 +0100

    Litt mer frontend lagt til

--- client/src/pages/Admin/Payment_Create/Payment_Create.jsx
+++ client/src/pages/Admin/Payment_Create/Payment_Create.jsx
@@ -2,12 +2,12 @@ import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { AuthContext } from '../../../context/authContext';
-import { Button, Label, TextInput } from 'flowbite-react';
-import DatePicker from 'react-datepicker'; 
-import 'react-datepicker/dist/react-datepicker.css'; 
+import { Button, Label, Select, TextInput } from 'flowbite-react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const Payment_Create = () => {
-  const axiosInstance = axios.create({baseURL: import.meta.env.VITE_REACT_APP_API_URL});
+  const axiosInstance = axios.create({ baseURL: import.meta.env.VITE_REACT_APP_API_URL });
   const navigate = useNavigate();
   const { currentUser } = useContext(AuthContext);
 
@@ -20,51 +20,61 @@ const Payment_Create = () => {
   }, [currentUser, navigate]);
 
   return (
-    <div className='h-auto flex justify-center items-center'>
+    <div className='h-screen flex justify-center items-center'>
       <div>
         <h1 className='text-center mt-8 text-2xl font-bold'>Opprette Regning</h1>
 
-        <div className='flex flex-col w-48 mt-20'>
-          <div className="mb-2 block">
-            <Label htmlFor="input-gray" color="gray" value="Fornavn" />
+        <div className='flex flex-col mt-8 space-y-4'>
+
+          <div className='flex space-x-4'>
+            <div className="flex flex-col">
+              <Label htmlFor="input-gray" color="gray" value="Fornavn" />
+              <TextInput id="input-gray" placeholder="Fornavn" required color="gray" />
+            </div>
+            <div className="flex flex-col">
+              <Label htmlFor="input-gray" color="gray" value="Etternavn" />
+              <TextInput id="input-gray" placeholder="Etternavn" required color="gray" />
+            </div>
+
+            <div className="flex flex-col">
+              <Label htmlFor="input-blue" color="blue" value="E-post" />
+              <TextInput id="input-blue" placeholder="E-post" required color="blue" />
+            </div>
+            <div className="flex flex-col">
+              <Label htmlFor="input-blue" color="blue" value="Telefon" />
+              <TextInput id="input-blue" placeholder="Telefon" required color="blue" />
+            </div>
           </div>
-          <TextInput id="input-gray" placeholder="Fornavn" required color="gray" />
 
-          <div className="mb-2 mt-4 block">
-            <Label htmlFor="input-gray" color="gray" value="Etternavn" />
+          <div className='flex space-x-4'>
+            <div className="flex flex-col">
+              <Label htmlFor="input-blue" color="blue" value="Vare" />
+              <Select id="countries" required>
+                  <option>Klubbhus Utleie</option>
+                  <option>Baneutleie</option>
+              </Select>
+            </div>
+            <div className="flex flex-col">
+              <Label htmlFor="input-blue" color="blue" value="Forfalls Dato" />
+              <DatePicker
+                id="input-date"
+                selected={selectedDate}
+                onChange={date => setSelectedDate(date)}
+                placeholderText="Velg dato"
+                dateFormat="dd.MM.yyyy"
+                className="form-control rounded-xl border-gray-200"
+              />
+            </div>
+            <div className="flex flex-col">
+              <Label htmlFor="input-blue" color="blue" value="Pris" />
+              <TextInput id="input-blue" placeholder="Pris" required color="blue" />
+            </div>
+            <div className="flex flex-col">
+              <Label htmlFor="input-blue" color="blue" value="Evt. Beskrivelse" />
+              <TextInput id="input-blue" placeholder="Beskrivelse" required color="blue" />
+            </div>
           </div>
-          <TextInput id="input-gray" placeholder="Etternavn" required color="gray" />
-
-          <div className="mb-2 mt-4 block">
-            <Label htmlFor="input-blue" color="blue" value="E-post" />
-          </div>
-          <TextInput id="input-blue" placeholder="E-post" required color="blue" />
-
-          <div className="mb-2 mt-4 block">
-            <Label htmlFor="input-blue" color="blue" value="Telefon" />
-          </div>
-          <TextInput id="input-blue" placeholder="Telefon" required color="blue" />
-
-          <div className="mb-2 mt-4 block">
-            <Label htmlFor="input-blue" color="blue" value="Forfalls Dato" />
-          </div>
-          <DatePicker
-            id="input-date"
-            selected={selectedDate}
-            onChange={date => setSelectedDate(date)}
-            placeholderText="Velg dato"
-            dateFormat="dd.MM.yyyy" // Customize date format if needed
-            className="form-control rounded-xl border-gray-200" // You might need to adjust the class for styling
-          />
-
-          <div className="mb-2 mt-4 block">
-            <Label htmlFor="input-blue" color="blue" value="Pris" />
-          </div>
-          <TextInput id="input-blue" placeholder="Pris" required color="blue" />
-
-          <Button color="dark" className="mt-6">Send Faktura</Button>
-          
-
+          <Button color="dark" className="mt-6 w-full">Send Faktura</Button>
         </div>
       </div>
     </div>
--- client/src/pages/Admin/Payment_Edit/Payment_Edit.jsx
+++ client/src/pages/Admin/Payment_Edit/Payment_Edit.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { AuthContext } from '../../../context/authContext';
+import { Table } from 'flowbite-react';
 
 const Payment_Edit = () => {
   const axiosInstance = axios.create({baseURL: import.meta.env.VITE_REACT_APP_API_URL,});
@@ -18,6 +19,57 @@ const Payment_Edit = () => {
   return (
     <>
       <h1 className='text-center mt-8 text-2xl font-bold'>Oversikt over Regninger</h1>
+
+      <div className="overflow-x-auto mt-20 px-8">
+        <Table striped>
+            <Table.Head>
+            <Table.HeadCell>Fornavn</Table.HeadCell>
+            <Table.HeadCell>Etternavn</Table.HeadCell>
+            <Table.HeadCell>E-post</Table.HeadCell>
+            <Table.HeadCell>Telefon</Table.HeadCell>
+            <Table.HeadCell>Vare</Table.HeadCell>
+            <Table.HeadCell>Pris</Table.HeadCell>
+            <Table.HeadCell>Beskrivelse</Table.HeadCell>
+            <Table.HeadCell>Forfalls Dato</Table.HeadCell>
+            <Table.HeadCell>Status</Table.HeadCell>
+            <Table.HeadCell>Re-send</Table.HeadCell>
+            <Table.HeadCell>Rediger</Table.HeadCell>
+            <Table.HeadCell>
+                <span className="sr-only">Edit</span>
+            </Table.HeadCell>
+            </Table.Head>
+            <Table.Body className="divide-y">
+            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <Table.Cell>Ola</Table.Cell>
+                <Table.Cell>Nordman</Table.Cell>
+                <Table.Cell>ola@gmail.com</Table.Cell>
+                <Table.Cell>45195555</Table.Cell>
+                <Table.Cell>Klubbhus Utleie</Table.Cell>
+                <Table.Cell>4000,-</Table.Cell>
+                <Table.Cell></Table.Cell>
+                <Table.Cell>14.02.2024</Table.Cell>
+                <Table.Cell>Betalt</Table.Cell>
+                <Table.Cell>Sende pånytt</Table.Cell>
+                <Table.Cell>Rediger</Table.Cell>
+            </Table.Row>
+            
+            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <Table.Cell>Ola</Table.Cell>
+                <Table.Cell>Nordman</Table.Cell>
+                <Table.Cell>ola@gmail.com</Table.Cell>
+                <Table.Cell>45195555</Table.Cell>
+                <Table.Cell>Klubbhus Utleie</Table.Cell>
+                <Table.Cell>4000,-</Table.Cell>
+                <Table.Cell></Table.Cell>
+                <Table.Cell>14.02.2024</Table.Cell>
+                <Table.Cell>Betalt</Table.Cell>
+                <Table.Cell>Sende pånytt</Table.Cell>
+                <Table.Cell>Rediger</Table.Cell>
+            </Table.Row>
+
+            </Table.Body>
+        </Table>
+       </div>
 
       
     </>
